25b931492be2dc2dc0ce9b9d7c548f913dea0c2f
Bruno Schultz <obruno.bbs@gmail.com>
2023-02-16 17:25:01 -0300

    Added initial screen and dependency injection

---
+++ app/src/main/java/com/example/mystockmarketapp/StockApplication.kt
@@ -0,0 +1,8 @@
+package com.example.mystockmarketapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class StockApplication: Application() {
+}
---
+++ app/src/main/java/com/example/mystockmarketapp/di/RepositoryModule.kt
@@ -0,0 +1,29 @@
+package com.example.mystockmarketapp.di
+
+import com.example.mystockmarketapp.data.csv.CSVParser
+import com.example.mystockmarketapp.data.csv.CompanyListingsParser
+import com.example.mystockmarketapp.data.repository.StockRepositoryImpl
+import com.example.mystockmarketapp.domain.model.CompanyListing
+import com.example.mystockmarketapp.domain.repository.StockRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindCompanyListingsParser(
+        companyListingsParser: CompanyListingsParser
+    ): CSVParser<CompanyListing>
+
+    @Binds
+    @Singleton
+    abstract fun bindStockRepository(
+        stockRepositoryImpl: StockRepositoryImpl
+    ): StockRepository
+}
